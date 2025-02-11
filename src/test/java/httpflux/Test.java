e03805e6cf6863e0c8c95742df7dbaee42b749d4
Almario Miano <almariomiano@gmail.com>
2025-02-11 11:13:13 +0800

    Update Test.java

--- src/test/java/httpflux/Test.java
+++ src/test/java/httpflux/Test.java
@@ -5,21 +5,30 @@ import com.amiano4.httpflux.HttpService;
 public class Test {
 	public static void main(String[] args) {
 
-		System.out.println("Test case 1: ");
+		try {
+			System.out.println("Test case 1: ");
 
-		HttpService.post("https://httpbin.org/post").onSuccess((response) -> {
-			System.out.print(response.body());
-		}).onError((response) -> {
-			System.out.print("error : " + response.getMessage());
-		});
+			HttpService.post("https://httpbin.org/post").onSuccess((response) -> {
+				System.out.print(response.body());
+			}).onError((response) -> {
+				System.out.print("error : " + response.getMessage());
+			}).executeSync();
 
-		System.out.println("Test case 2: ");
+			System.out.println("Test case 2: ");
 
-		HttpService.get("https://httpbin.org/post").onSuccess((response) -> {
-			System.out.println(response.body());
-		}).onError((error) -> {
-			// System.out.println(error.getResponse().body());
-			error.printStackTrace();
-		});
+			HttpService.get("https://httpbin.org/post").onSuccess((response) -> {
+				System.out.println(response.body());
+			}).onError((error) -> {
+				// System.out.println(error.getResponse().body());
+				error.printStackTrace();
+			}).executeAsync();
+
+			Thread.sleep(3000);
+
+			System.out.println("\nTest end ");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
